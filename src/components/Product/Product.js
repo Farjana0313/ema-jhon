3ef3d5543753854b82ed6719e862ebf7c33c04f9
Farjana0313 <30821829+Farjana0313@users.noreply.github.com>
2023-02-10 08:59:06 +0600

    update ema-jhon

--- src/components/Product/Product.js
+++ src/components/Product/Product.js
@@ -1,10 +1,26 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faCartShopping} from '@fortawesome/free-solid-svg-icons';
 import './Product.css';
 
-const Product = () => {
+
+const Product = (props) => {
+    console.log(props.product.name);
+    const { img, name, seller, price, stock } = props.product;
     return (
-        <div>
-            <h3>this is product</h3>
+        <div className="product">
+            <div>
+                <img src={img} alt="" />
+            </div>
+            <div>
+                <h4 className="product-name">{name}</h4>
+                <br />
+                <p><small>by: {seller} </small></p>
+                <p>${price}</p>
+                <p><small>Only {stock} left in stock - Order soon</small></p>
+                <button className="main-button" onClick={()=>props.handleAddProduct(props.product)}><FontAwesomeIcon icon={faCartShopping} /> add to cart</button>
+            </div>
+
         </div>
     );
 };
--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -1,21 +1,27 @@
 import React, { useState } from 'react';
 import fakeData from '../../fakeData';
 import './Shop.css'
+import Product from '../Product/Product';
+import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const frist10 = fakeData.slice(0, 10);
     const [products, setProducts] = useState(frist10);
+    const [cart, setCart] = useState([]);
+
+    const handleAddProduct = (product) => {
+        const newCart = [...cart, product];
+        setCart(newCart);
+    }
     return (
         <div className="shop-container">
             <div className="product-container">
-                <ul>
-                    {
-                        products.map(product => <li>{product.name}</li>)
-                    }
-                </ul>
+                {
+                    products.map(product => <Product product={product} handleAddProduct={handleAddProduct}>{product.name}</Product>)
+                }
             </div>
             <div className="cart-container">
-                <h3>this is cart container</h3>
+                <Cart cart={cart}></Cart>
             </div>
 
         </div>
